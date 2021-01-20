e7d66d7f1d26cb47ae58a56ba7282a661822fcb8
Maksym Taratuta <taratutamaxim@gmail.com>
2021-01-20 13:34:09 +0200

    add: Typogrphy.jsx

--- src/themes/theme.js
+++ src/themes/theme.js
@@ -36,6 +36,40 @@ const theme = {
       lg: tw`px-6 py-3 text-base`,
     },
   },
+  typography: {
+    title: {
+      default: {
+        base: tw`tracking-tight font-extrabold`,
+        color: tw`text-gray-900`,
+        size: tw`text-4xl sm:text-5xl md:text-6xl`,
+      },
+    },
+    subtitle: {
+      default: {
+        base: tw``,
+        color: tw`text-gray-500`,
+        size: tw`text-base sm:text-lg md:text-xl`,
+      },
+    },
+    paragraph: {},
+    text: {
+      default: {
+        base: tw``,
+      },
+    },
+    menu: {
+      default: {
+        base: tw`font-medium`,
+        color: tw`text-gray-500 hover:text-gray-900`,
+        size: tw`text-base sm:text-lg md:text-xl`,
+      },
+    },
+    code: {
+      default: {
+        base: tw``,
+      },
+    },
+  },
 };
 
 export default theme;
