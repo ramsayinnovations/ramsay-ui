03bfe366fb020456d090c6df6af1d9cea2ba2363
Moshe Morris <moshem@live.com>
2021-01-20 21:30:42 +0200

    Various updates

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,42 +1,39 @@
 import PropTypes from "prop-types";
 import tw from "twin.macro";
-import theme from "@/themes/theme";
 import styled from "styled-components";
+import button from "@/themes/button";
 
 const Button = styled.button(
-  ({ className, styles: { block, color, border, shape, size } = {} }) => [
-    theme.button.default.base,
-    theme.button.default.color,
-    theme.button.default.hover,
-    theme.button.default.focus,
+  ({ block, border, color, shape, size, className }) => [
+    button.default.base,
+    button.default.color,
+    button.default.hover,
+    button.default.focus,
+    button.border.base,
     block && tw`w-full`,
-    color && theme.button.color[color],
-    theme.button.border.base,
-    border && theme.button.border[border],
-    shape && theme.button.borderRadius[shape],
-    size && theme.button.size[size],
+    border && button.border[border],
+    color && button.color[color],
+    shape && button.borderRadius[shape],
+    size && button.size[size],
     className,
   ]
 );
 
 Button.propTypes = {
-  className: PropTypes.any,
   children: PropTypes.string,
-  as: PropTypes.string,
-  styles: PropTypes.shape({
-    block: PropTypes.string,
-    border: PropTypes.oneOf([
-      "gray",
-      "grey",
-      "transparent",
-      "primary",
-      "success",
-      "danger",
-    ]),
-    color: PropTypes.oneOf(["primary", "success", "danger"]),
-    shape: PropTypes.oneOf(["flat", "pill", "rounded"]),
-    size: PropTypes.oneOf(["xs", "sm", "md", "lg", "xl"]),
-  }),
+  className: PropTypes.any,
+  block: PropTypes.string,
+  border: PropTypes.oneOf([
+    "gray",
+    "grey",
+    "transparent",
+    "primary",
+    "success",
+    "danger",
+  ]),
+  color: PropTypes.oneOf(["primary", "success", "danger"]),
+  shape: PropTypes.oneOf(["flat", "pill", "rounded"]),
+  size: PropTypes.oneOf(["xs", "sm", "md", "lg", "xl"]),
 };
 
 export default Button;
