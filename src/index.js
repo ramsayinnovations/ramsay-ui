cb142ae8f8e4c3d61492282c2ba7f9788cacd9a5
Maksym Taratuta <taratutamaxim@gmail.com>
2021-01-26 10:58:44 +0200

    update: Hero component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
+import HeroLeft from "./components/hero/HeroLeft";
+import HeroRight from "./components/hero/HeroRight";
+
 export * as Button from "@components/button/Button";
 export * as Code from "@components/typography/Code";
 export * as Text from "@components/typography/Text";
 export * as Title from "@components/typography/Title";
 export * as Hero from "@components/block/hero/Hero";
+export * as HeroLeft from "@components/block/hero/HeroLeft";
+export * as HeroRight from "@components/block/hero/HeroRight";
