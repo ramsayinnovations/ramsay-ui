c59e726b6d36edcdc0c1316ac7b3721ccae2732c
Maksym Taratuta <taratutamaxim@gmail.com>
2021-01-26 16:17:27 +0200

    add: Card and CardItem

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@ export * as Button from "@components/button/Button";
 export * as Code from "@components/typography/Code";
 export * as Text from "@components/typography/Text";
 export * as Title from "@components/typography/Title";
+export * as Card from "@components/card/Card";
+export * as CardItem from "@components/card/CardItem";
--- src/themes/theme.js
+++ src/themes/theme.js
@@ -1,9 +1,11 @@
 import typography from "./typography/typography";
 import button from "./button/button";
+import card from "./card/card";
 
 const theme = {
   typography: typography,
   button: button,
+  card: card,
 };
 
 export default theme;
