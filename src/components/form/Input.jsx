836193ad8004c1d7cd337e2c69a0e2237806977a
Maksym Taratuta <taratutamaxim@gmail.com>
2021-01-27 17:10:39 +0200

    update: Input component

--- src/components/form/Input.jsx
+++ src/components/form/Input.jsx
@@ -4,6 +4,8 @@ import theme from "@/themes/theme";
 const Input = ({
   classes = {},
   labelText,
+  optionalText,
+  optionalTextId,
   placeholder,
   name,
   id,
@@ -14,28 +16,40 @@ const Input = ({
   errorTextId,
 }) => (
   <div css={[classes.wrapper]}>
-    {labelText && (
-      <label htmlFor={name} css={[theme.input.default.label, classes.label]}>
-        {labelText}
-      </label>
-    )}
-    <div css={[theme.input.default.inputWrapper, classes.inputWrapper]}>
+    <div
+      css={[
+        theme.input.labelWrapper,
+        optionalText && theme.input.optionalWrapper,
+      ]}
+    >
+      {labelText && (
+        <label htmlFor={name} css={[theme.input.label, classes.label]}>
+          {labelText}
+        </label>
+      )}
+      {optionalText && (
+        <span css={[theme.input.optional]} id={optionalTextId}>
+          {optionalText}
+        </span>
+      )}
+    </div>
+    <div css={[theme.input.inputWrapper, classes.inputWrapper]}>
       <input
         type="text"
         name={name}
         id={id}
         css={[
-          error ? theme.input.default.inputError : theme.input.default.input,
+          error ? theme.input.inputError : theme.input.input,
           classes.input,
         ]}
         placeholder={placeholder}
         ariaInvalid={!!error}
       />
       {error && (
-        <div css={[theme.input.default.error, classes.error]}>
+        <div css={[theme.input.error, classes.error]}>
           {/* <!-- Heroicon name: exclamation-circle --> */}
           <svg
-            css={[theme.input.default.errorSvg, classes.errorSvg]}
+            css={[theme.input.errorSvg, classes.errorSvg]}
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 20 20"
             fill="currentColor"
@@ -51,15 +65,12 @@ const Input = ({
       )}
     </div>
     {helpText && (
-      <p css={[theme.input.default.helpText, classes.helpText]} id={helpTextId}>
+      <p css={[theme.input.helpText, classes.helpText]} id={helpTextId}>
         {helpText}
       </p>
     )}
     {errorText && (
-      <p
-        css={[theme.input.default.errorText, classes.errorText]}
-        id={errorTextId}
-      >
+      <p css={[theme.input.errorText, classes.errorText]} id={errorTextId}>
         {errorText}
       </p>
     )}
@@ -78,6 +89,8 @@ Input.propTypes = {
     errorText: PropTypes.string,
   }),
   labelText: PropTypes.string,
+  optionalText: PropTypes.string,
+  optionalTextId: PropTypes.string,
   placeholder: PropTypes.string,
   name: PropTypes.string,
   id: PropTypes.string,
