d755af6709e67fd54f51266d0a36155c1af07fd0
Maksym Taratuta <taratutamaxim@gmail.com>
2021-01-27 17:00:20 +0200

    add: Select component

--- src/themes/theme.js
+++ src/themes/theme.js
@@ -2,12 +2,14 @@ import typography from "./typography/typography";
 import button from "./button/button";
 import input from "./form/input";
 import textarea from "./form/textarea";
+import select from "./form/select";
 
 const theme = {
   typography: typography,
   button: button,
   input: input,
   textarea: textarea,
+  select: select,
 };
 
 export default theme;
