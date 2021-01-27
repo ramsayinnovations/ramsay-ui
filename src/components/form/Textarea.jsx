3ecd857d88723a8d9a3da23794eee17bd3a5603d
Maksym Taratuta <taratutamaxim@gmail.com>
2021-01-27 13:11:50 +0200

    add: Textarea component

---
+++ src/components/form/Textarea.jsx
@@ -0,0 +1,54 @@
+import PropTypes from "prop-types";
+import theme from "@/themes/theme";
+
+const Textarea = ({
+  classes = {},
+  label,
+  helpText,
+  id,
+  name,
+  rows,
+  placeholder,
+}) => (
+  <div css={[classes.wrapper]}>
+    {label && (
+      <label htmlFor={name} css={[theme.textarea.default.label, classes.label]}>
+        {label}
+      </label>
+    )}
+    <div
+      css={[theme.textarea.default.textareaWrapper, classes.textareaWrapper]}
+    >
+      <textarea
+        id={id}
+        name={name}
+        rows={rows || 3}
+        css={[theme.textarea.default.textarea, classes.textarea]}
+        placeholder={placeholder}
+      ></textarea>
+    </div>
+    {helpText && (
+      <p css={[theme.textarea.default.helpText, classes.helpText]}>
+        {helpText}
+      </p>
+    )}
+  </div>
+);
+
+Textarea.propTypes = {
+  classes: PropTypes.shape({
+    wrapper: PropTypes.string,
+    label: PropTypes.string,
+    textareaWrapper: PropTypes.string,
+    textarea: PropTypes.string,
+    helpText: PropTypes.string,
+  }),
+  label: PropTypes.string,
+  helpText: PropTypes.string,
+  id: PropTypes.string,
+  name: PropTypes.string,
+  rows: PropTypes.number,
+  placeholder: PropTypes.string,
+};
+
+export default Textarea;
--- src/themes/theme.js
+++ src/themes/theme.js
@@ -1,11 +1,13 @@
 import typography from "./typography/typography";
 import button from "./button/button";
 import input from "./form/input";
+import textarea from "./form/textarea";
 
 const theme = {
   typography: typography,
   button: button,
   input: input,
+  textarea: textarea,
 };
 
 export default theme;
