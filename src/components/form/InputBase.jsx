9665eb9b3b5b4b827053e3a95dba110633fcf9ab
Moshe Morris <moshem@live.com>
2021-01-28 10:57:29 +0200

    Added comment about `col` prop to InputBase.jsx

--- src/components/form/InputBase.jsx
+++ src/components/form/InputBase.jsx
@@ -5,36 +5,58 @@ const {
   form: { input },
 } = theme;
 
-const InputBase = ({ className, hasError, wrapper, name, placeholder }) => (
-  <div className={wrapper}>
-    <input
-      type="text"
-      name={name}
-      id={name}
-      css={[
-        input.base,
-        input.border,
-        input.size,
-        input.space,
-        input.focus,
-        input.error,
-        hasError && input.error,
-        className,
-      ]}
-      placeholder={placeholder}
-      ariaInvalid={!!hasError}
-      ariaDescribedby={`${name}-error`}
-    />
-  </div>
-);
+const InputBase = ({
+  className,
+  col,
+  hasError,
+  wrapper,
+  name,
+  placeholder,
+}) => {
+  // I want to create a `col` prop that will output tailwindcss classes to the base div as follows:
+  // `col === "1": `col-span-6`
+  // `col === "1/2"`: `col-span-6 sm:col-span-3`
+  // `col === "1/3"`: `col-span-6 sm:col-span-3 lg:col-span-2`
+  // `col === "2/3": `col-span-6 sm:col-span-4`
+
+  // alternative syntax:
+
+  // `col === "full": same as `col === "1"
+  // `col === "half": same as `col === "1/2"
+  // `col === "third": same as `col === "1/3"
+  // `col === "two-thirds": same as `col === "2/3"
+
+  return (
+    <div className={[wrapper]}>
+      <input
+        type="text"
+        name={name}
+        id={name}
+        css={[
+          input.base,
+          input.border,
+          input.size,
+          input.space,
+          input.focus,
+          input.error,
+          hasError && input.error,
+          className,
+        ]}
+        placeholder={placeholder}
+        ariaInvalid={!!hasError}
+        ariaDescribedby={`${name}-error`}
+      />
+    </div>
+  );
+};
 
 InputBase.propTypes = {
-  children: PropTypes.any,
   className: PropTypes.string,
+  col: PropTypes.string,
   hasError: PropTypes.bool,
+  wrapper: PropTypes.string,
   name: PropTypes.string,
   placeholder: PropTypes.string,
-  wrapper: PropTypes.string,
 };
 
 export default InputBase;
