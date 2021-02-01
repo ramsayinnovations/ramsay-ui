28fc2cef980decbb1ff17ba1ace6cf5fdb05434c
Maksym Taratuta <taratutamaxim@gmail.com>
2021-02-01 16:09:05 +0200

    fix: imports

--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,7 @@ import Code from "./typography/Code";
 import Text from "./typography/Text";
 import Title from "./typography/Title";
 import Input from "./form/Input";
-import Select from "./from/Select";
-import Textarea from "./from/Textarea";
+import Select from "./form/Select";
+import Textarea from "./form/Textarea";
 
 export { Button, Code, Text, Title, Input, Select, Textarea };
