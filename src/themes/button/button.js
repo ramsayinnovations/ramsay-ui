eac39e2e4b9609d4d6c00fb7c1e2ce13d4115d9b
Maksym Taratuta <taratutamaxim@gmail.com>
2021-02-01 12:07:34 +0200

    fix: white color

--- src/themes/button/button.js
+++ src/themes/button/button.js
@@ -1,11 +1,11 @@
 import tw from "twin.macro";
 
-const borderGrey = tw`border-gray-300 text-gray-700 bg-white-100 hover:bg-gray-50`;
+const borderGrey = tw`border-gray-300 text-gray-700 bg-white hover:bg-gray-50`;
 
 const button = {
   default: {
     base: tw`inline-flex items-center shadow-sm font-medium`,
-    color: tw`text-white-100 bg-indigo-600`,
+    color: tw`text-white bg-indigo-600`,
     hover: tw`hover:bg-indigo-700`,
     focus: tw`focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`,
   },
