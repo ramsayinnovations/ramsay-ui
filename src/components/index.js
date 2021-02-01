a48e56107510b1468edd07b4defdd10a5eb8d0ec
Maksym Taratuta <taratutamaxim@gmail.com>
2021-02-01 15:59:48 +0200

    fix: exports

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,8 @@ import Button from "./button/Button";
 import Code from "./typography/Code";
 import Text from "./typography/Text";
 import Title from "./typography/Title";
+import Input from "./form/Input";
+import Select from "./from/Select";
+import Textarea from "./from/Textarea";
 
-export { Button, Code, Text, Title };
+export { Button, Code, Text, Title, Input, Select, Textarea };
