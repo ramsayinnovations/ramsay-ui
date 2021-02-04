facb4bdecce537677245318c213c8e82d0a136eb
Maksym Taratuta <taratutamaxim@gmail.com>
2021-02-04 10:39:00 +0200

    fix: exports

---
+++ src/components/index.js
@@ -0,0 +1,45 @@
+import Avatar from "./avatar/avatar";
+import Button from "./button/Button";
+import Card from "./card/Card";
+import CardItem from "./card/CardItem";
+import Form from "./form/Form";
+import Input from "./form/Input";
+import Select from "./form/Select";
+import Textarea from "./form/Textarea";
+import Hero from "./block/hero/Hero";
+import HeroLeft from "./block/hero/HeroLeft";
+import HeroRight from "./block/hero/HeroRight";
+import Menu from "./menu/Menu";
+import MenuLink from "./menu/MenuLink";
+import Desktop from "./navbar/Desktop";
+import Hamburger from "./navbar/Hamburger";
+import Logo from "./navbar/Logo";
+import Mobile from "./navbar/Mobile";
+import Navbar from "./navbar/Navbar";
+import Code from "./typography/Code";
+import Text from "./typography/Text";
+import Title from "./typography/Title";
+
+export {
+  Avatar,
+  Button,
+  Card,
+  CardItem,
+  Form,
+  Input,
+  Select,
+  Textarea,
+  Hero,
+  HeroLeft,
+  HeroRight,
+  Menu,
+  MenuLink,
+  Desktop,
+  Hamburger,
+  Logo,
+  Mobile,
+  Navbar,
+  Code,
+  Text,
+  Title,
+};
