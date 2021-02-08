927a717bb205ebf8fbc29992b00428c4f9daa62b
Maksym Taratuta <taratutamaxim@gmail.com>
2021-02-08 13:23:14 +0200

    fix: Textarea default resize

--- src/themes/form/textarea.js
+++ src/themes/form/textarea.js
@@ -2,7 +2,7 @@ import tw from "twin.macro";
 
 const textarea = {
   wrapper: tw`mt-1`,
-  base: tw`shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md`,
+  base: tw`shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md resize-none`,
 };
 
 export default textarea;
