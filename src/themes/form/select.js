ce0899548827d872e4bf7360ee82f25ba24a4487
Maksym Taratuta <taratutamaxim@gmail.com>
2021-02-17 14:23:24 +0200

    fix: select color

--- src/themes/form/select.js
+++ src/themes/form/select.js
@@ -8,7 +8,7 @@ const select = {
   buttonWrapper: tw`inline-block w-full rounded-md shadow-sm`,
   button: {
     base: tw`cursor-default relative w-full rounded-md border border-gray-300 bg-white pl-3 pr-10 py-2 text-left focus:outline-none focus:shadow focus:border-blue-300 transition ease-in-out duration-150 sm:text-sm sm:leading-5`,
-    value: tw`block truncate`,
+    value: tw`block truncate text-black`,
     iconWrapper: tw`absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none`,
     icon: tw`h-5 w-5 text-gray-400`,
   },
